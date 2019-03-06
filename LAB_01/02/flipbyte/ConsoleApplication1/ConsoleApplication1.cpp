1b7fb01cf9f3f94e90a2a1ba26ecde3c3a872f7b
StrangerAleks <Stranger_aleks@mail.ru>
2019-03-06 15:50:35 +0300

    лаюораторная 01_02 рабочая

--- LAB_01/02/flipbyte/ConsoleApplication1/ConsoleApplication1.cpp
+++ LAB_01/02/flipbyte/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,53 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
+char BIT(const char N) {return 1 << N;}
+
+signed short strToByte(const char *p)
 {
-    std::cout << "Hello World!\n"; 
+	signed short Byte = 0;
+	while (*p != '\0')	{		if ((*p >= '0') && (*p <= '9'))		{			Byte = Byte * 10 + (unsigned)*p - (unsigned) '0';			if (Byte > 255) return (-1);		}		else			return (-1);		p++;	}
+	return Byte;
+}
+
+char flipbyte(const char byte)
+{
+	char result = 0;
+	for (char i = 0; i < 8; i++)
+	{
+		if (byte & BIT(i)) result |= BIT(7 - i);
+	}
+	return result;
+}
+
+bool check_args_ok(const int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Invalid argument count\n"
+			<< "Usage: flipbyte <string>\n";
+		return false;
+	} 
+	else
+		return true;
+}
+
+int main(int argc, char* argv[])
+{
+	if (check_args_ok(argc, &argv[0]))
+	{
+		signed short result = strToByte(argv[1]);
+		if (result < 0)
+			return 1;
+		else
+		{
+			unsigned short byte = flipbyte(result) & 0x00FF;
+			cout << byte;
+			return 0;
+		}
+	}
+	else
+		return 1;
 }
